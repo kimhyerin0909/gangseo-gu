2cfae4e5d32d16408cbeb29f6bcb9abf49a31730
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-07 11:20:56 +0900

    new

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+// import CctvMarker from './components/CctvMarker';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
---
+++ front/src/components/CctvMarker.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import axios from 'axios';
+
+export default function CctvMarker() {
+    // const url = "http://apis.data.go.kr/6260000/fbusangmgsafeinfo/getgmgcctvinfo?serviceKey=FyM5ob%2BBFyX4GsZiEnANdaCxNxsWATlQIMRg4EuWkZvJdLmikYCng%2BZtvWQpNXFWOoGQOEveFkM5w7SPMV5lTw%3D%3D";
+    
+    // const getData = async () => {
+    //     const res = await axios.get(url)
+    //     const data = res.data;
+    //     console.log(data);
+    // }
+
+    // useEffect(() => {
+    //     getData();
+    // }, [])
+    return (
+        <div>cctv</div>
+    )
+}
