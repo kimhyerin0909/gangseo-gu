bb559b68ba71beb6876deffa633ffc0c0ea3b339
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-09 20:37:18 +0900

    ETC :: delect useless div

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
+import './style.css';
 import Cards from './components/Cards';
-// import CctvMarker from './components/CctvMarker';
 import Header from './components/Header';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <div>
-        <Cards />
-      </div>
+      <Cards />
     </div>
   );
 }
