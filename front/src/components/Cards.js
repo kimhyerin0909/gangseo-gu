1ddb717f17c6fe9a0fc3fc25448c50f7210c7fb0
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-09 20:38:10 +0900

    FEAT :: add scroll box and star point

--- front/src/components/Cards.js
+++ front/src/components/Cards.js
@@ -1,28 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import ScrollHorizontal from 'react-scroll-horizontal';
 import axios from 'axios';
 
 export default function Cards() {
     const [card, setCard] = useState([]);
+    const fullStarImg = useRef();
     const getCardData = async () => {
         const response = await axios.get("/api/places")
         .then(res => res.data);
         setCard(response)
     }
+
+    const showStarPoint = () => {
+        let point = card["score"];
+        point = point * 20;
+        // console.log(fullStarImg.current.style)
+        // fullStarImg.current.style = `width : ${point}px`
+    }
+
     useEffect(() => {
         getCardData();
+        showStarPoint();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
     return (
-        <div>
-            {card.map(data => {
-                const a = <div className={`card${data["place_id"]}`} key={data["place_id"]}>
-                <img alt='images' src={data["img_url"]} />
-                    <span>{data["place_name"]}</span>
-                    <span>{data["description"]}</span>
-                    <span>{data["score"]}</span>
-                </div>
-                return a;
-            })
-            }
+        <div id='scroll-horizontal' className='card-box' >
+            <ScrollHorizontal >
+                {card.map(data => {
+                    const a = <div className={`card card${data["place_id"]}`} key={data["place_id"]} >
+                        <img className='place_img' alt='images' src={data["img_url"]} />
+                        <span className='place_name'>{data["place_name"]}</span>
+                        <span className='place_desc'>{data["description"]}</span>
+                        <section>
+                            <img ref={fullStarImg} className='star full-star' alt='star' src='../full_star.png'/>
+                            <img className='star' alt='star' src='../empty_star.png'/>
+                            <span className='star-point'>{data["score"]}</span>
+                        </section>
+                    </div>
+                    return a;
+                })
+                }
+            </ScrollHorizontal>
         </div>
     )
 }
