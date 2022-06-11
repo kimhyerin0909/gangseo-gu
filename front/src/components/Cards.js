8404fc6106cb6f811fbda85fccd88909814d56d7
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-11 23:13:59 +0900

    FEAT :: show a score in a star
    점수를 별점 나타내기 기능

--- front/src/components/Cards.js
+++ front/src/components/Cards.js
@@ -1,26 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect,useRef,useState } from 'react'
 import ScrollHorizontal from 'react-scroll-horizontal';
 import axios from 'axios';
-
 export default function Cards() {
+    const star = useRef()
     const [card, setCard] = useState([]);
-    const fullStarImg = useRef();
     const getCardData = async () => {
         const response = await axios.get("/api/places")
-        .then(res => res.data);
-        setCard(response)
-    }
-
-    const showStarPoint = () => {
-        let point = card["score"];
-        point = point * 20;
-        // console.log(fullStarImg.current.style)
-        // fullStarImg.current.style = `width : ${point}px`
+        .then(res => {
+            setCard(res.data);
+        });
+        return response;
     }
 
     useEffect(() => {
         getCardData();
-        showStarPoint();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
@@ -31,11 +24,17 @@ export default function Cards() {
                     const a = <div className={`card card${data["place_id"]}`} key={data["place_id"]} >
                         <img className='place_img' alt='images' src={data["img_url"]} />
                         <span className='place_name'>{data["place_name"]}</span>
-                        <span className='place_desc'>{data["description"]}</span>
                         <section>
-                            <img ref={fullStarImg} className='star full-star' alt='star' src='../full_star.png'/>
-                            <img className='star' alt='star' src='../empty_star.png'/>
+                            <div className='stars'>
+                                <div style={{
+                                    backgroundPosition:`-${100 -(data["score"] * 20)}px`,
+                                    marginLeft:`-${100-(data["score"] * 20)}px`}
+                                    }
+                                    ref={star}className='full-star' alt='star' src='../full_star.png'></div>
+                                <div className='empty-star' alt='star' src='../empty_star.png'></div>
+                            </div>
                             <span className='star-point'>{data["score"]}</span>
+                            <span className='place_desc'>({data["description"]})</span>
                         </section>
                     </div>
                     return a;
