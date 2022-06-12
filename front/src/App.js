be901b37aacc6dd831e79365c85be2f0e93d768d
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-12 22:20:12 +0900

    FEAT :: Interlink links

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,23 @@
 import './style.css';
 import Cards from './components/Cards';
 import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandMark from './components/LandMark';
+import CctvMarker from './components/CctvMarker';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
-      <Cards />
+      <BrowserRouter>
+        <Header />
+        <Cards />
+        <Routes>
+          <Route exact path='/' element={<Home />}/>
+          <Route exact path="/landmark" element={<LandMark />}/>
+          <Route exact path="/cctv" element={<CctvMarker />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <NavLink to="/">홈</NavLink>
+      <NavLink to="/landmark">랜드마크</NavLink>
+      <NavLink to="/cctv">CCTV 위치</NavLink>
+    </div>
   )
 }
