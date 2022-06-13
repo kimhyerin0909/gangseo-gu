35d5093348c83040bb7323dbf90d993491bdf432
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-13 19:55:03 +0900

    FEAT :: add a logo and navlink
    로고 사진을 추가하고 시멘틱 태그를 사용해 명시적으로 바꿈

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
-    <div className='header'>
-      <NavLink to="/">홈</NavLink>
-      <NavLink to="/landmark">랜드마크</NavLink>
-      <NavLink to="/cctv">CCTV 위치</NavLink>
-    </div>
+    <header className='header'>
+      <section className='header-section'>
+        <img className='header-logo' alt='home' src='../adeotnr_logo.png' />
+        <nav className='header-nav'>
+          <NavLink to="/">홈</NavLink>
+          <NavLink to="/landmark">랜드마크</NavLink>
+          <NavLink to="/cctv">CCTV 위치</NavLink>
+        </nav>
+      </section>
+    </header>
   )
 }
