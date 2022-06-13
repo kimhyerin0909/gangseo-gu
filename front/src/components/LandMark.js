9af063ac018e085fa4b996ce5d202ef0a379a4e1
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-13 19:56:14 +0900

    FEAT :: show a marker that express landmarks with kakao map api
    카카오 맵 api를 사용해 랜드마크를 마커로 표시 -> 임시 데이터 사용

--- front/src/components/LandMark.js
+++ front/src/components/LandMark.js
@@ -1,21 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from './Cards';
-import lm from '../landmark.json';
+import lm from "../landmark.json";
+const { kakao } = window;
 
 export default function LandMark() {
-  const [landmarks, setLandmarks] = useState(lm);
-  console.log(landmarks)
+  function makeOverListener(map, marker, infowindow) {
+    return function() {
+        infowindow.open(map, marker);
+    };
+  }
+
+  // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+  function makeOutListener(infowindow) {
+      return function() {
+          infowindow.close();
+      };
+  }
+
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center : new kakao.maps.LatLng(35.1539342346421, 128.88044789823067),
+      level : 9
+    };
+    const map = new kakao.maps.Map(container, options);
+    const positions = lm.map(data => {
+      const a = {
+        id : data["id"],
+        title : data["name"],
+        latlng : new kakao.maps.LatLng(data["lat"], data["lon"]),
+        content : data["desc"],
+        imgUrl : data["img_url"],
+        clickable: true
+      }
+      return a;
+    }) 
+    for (var i = 0; i < positions.length; i ++) {
+      var marker = new kakao.maps.Marker({
+          map: map, // 마커를 표시할 지도
+          position: positions[i].latlng, // 마커를 표시할 위치
+          title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      });
+      var infowindow = new kakao.maps.InfoWindow({
+        content: `<div className="landmark-info">
+        <span>${positions[i].title}</span>
+      </div>`,
+      });
+      kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+      kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+    }
+    
+  }, [])
   return (
     <div>
-    <Cards />
+      <Cards />
       <article className='landmark'>
-        <p>landmark</p>
-        <nav className='lm-btns'>
-          {landmarks.map(data => {
-            const a = <button>{data["name"]}</button>
-            return a;
-          })}
-        </nav>
+        <p className='subtitle'>강서구 랜드마크</p>
+        <div id="map" style={{
+          width:'100%',
+          height:'500px'
+        }}></div>
       </article>
     </div>
     
