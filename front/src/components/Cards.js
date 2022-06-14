7de09e35c399f9299698a4a788304e8e7f7249f3
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-14 22:39:49 +0900

    ETC :: remove useRef, move scroll element's locate

--- front/src/components/Cards.js
+++ front/src/components/Cards.js
@@ -1,8 +1,7 @@
-import React, { useEffect,useRef,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ScrollHorizontal from 'react-scroll-horizontal';
 import axios from 'axios';
 export default function Cards() {
-    const star = useRef()
     const [card, setCard] = useState([]);
     const [isEmpty, setIsEmpty] = useState(false);
 
@@ -17,29 +16,30 @@ export default function Cards() {
     return (
         <div id='scroll-horizontal' className='card-box' >
             <p className='subtitle'>이곳에 가보시는 건 어떠신가요?</p>
+            
+            <ScrollHorizontal >
             {isEmpty ? <div className='data-empty'>데이터와 아리마셍데스</div>
                     : card.map(data => {
                 const a = 
-                <ScrollHorizontal >
                     <div className={`card card${data["place_id"]}`} key={data["place_id"]} >
                         <img className='place_img' alt='images' src={data["img_url"]} />
                         <span className='place_name'>{data["place_name"]}</span>
                         <section>
                             <div className='stars'>
                                 <div style={{
-                                    backgroundPosition:`-${98 -(data["score"] * 20)}px`,
-                                    marginLeft:`-${98-(data["score"] * 20)}px`}
+                                    backgroundPosition:`-${99.5 -(data["score"] * 20)}px`,
+                                    marginLeft:`-${99.5-(data["score"] * 20)}px`}
                                     }
-                                    ref={star}className='full-star' alt='star' src='../full_star.png'></div>
+                                    className='full-star' alt='star' src='../full_star.png'></div>
                                 <div className='empty-star' alt='star' src='../empty_star.png'></div>
                             </div>
                             <span className='star-point'>{data["score"]}</span>
                             <span className='place_desc'>({data["description"]})</span>
                         </section>
                     </div>
-                </ScrollHorizontal>
                 return a; })
             }
+            </ScrollHorizontal>
         </div>
     )
 }
