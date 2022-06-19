54a1fcc3180807b048a07a02a8529d9d75ae9b72
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-19 19:16:25 +0900

    ETC :: 필요없는 코드 삭제

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import './style.css';
 import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandMark from './components/LandMark';
-import CctvMarker from './components/CctvMarker';
 import Home from './components/Home';
 import Footer from './components/Footer';
 
@@ -15,7 +14,6 @@ export default function App() {
           <Routes>
             <Route exact path='/' element={<Home />}/>
             <Route exact path="/landmark" element={<LandMark />}/>
-            <Route exact path="/cctv" element={<CctvMarker />}/>
           </Routes>
         </main>
         <Footer />
