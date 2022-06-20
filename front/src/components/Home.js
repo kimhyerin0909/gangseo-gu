368917179bdfa8b6213b05e342296bd8617ebe0b
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-20 22:25:01 +0900

    FEAT :: sliding image, title and video
    슬라이더랑 제목, 유튜브 홍보영상 추가

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Home() {
+  const [slide, setSlide] = useState(1);
+  const [link, setLink] = useState();
+  const imgUrl = `https://www.bsgangseo.go.kr/visit/img/main/main_vis_0${slide}.jpg`;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(slide === 9) setSlide(num => 1);
+      setSlide(num => num + 1);
+    }, 4000)
+    return () => clearInterval(interval);
+  }, [slide])
+
+  const linkToLandmark = () => {
+    setLink(() => <NavLink onMouseLeave={downLink} to="/landmark" className="link-to-lm"><span className='link'>더 자세한 정보를 보고 싶다면?</span></NavLink>)
+  }
+
+  const downLink = () => {
+    setLink("");
+  }
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home-img'>
+        {link}
+        <img onMouseOver={linkToLandmark} className='main-img' alt="gangseo-gu" src={imgUrl} />
+      </div>
+      <div className='home-main'>
+        <h2>부산광역시 강서구</h2>
+        <iframe className='youtube' width="560" height="315" src="https://www.youtube.com/embed/i_iUoLdpOZ8?controls=0?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
   )
 }
